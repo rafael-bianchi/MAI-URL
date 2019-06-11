5a597f57012e7b97ab98f6578aaeb9576d4b75e5
Rafael Bianchi <rafaellfontana@gmail.com>
2019-06-11 16:27:51 +0200

    kmeans ++ and added pen-based dataset

--- UL/run.py
+++ UL/run.py
@@ -1,69 +1,115 @@
+#%% Imports
+import time
+
 import numpy as np
+import pandas as pd
 import scipy.spatial.distance as dist
 from sklearn.cluster import KMeans
 from sklearn.metrics.cluster import normalized_mutual_info_score
+from tabulate import tabulate
 
 import ul_project.model.minmax_kmeans as mk
 import ul_project.utils.utils as utils
 
-X, Y, N, d, k_read = utils.read_dataset('Coil2.csv')
-
+#%%
 # MinMax Algorithm parameters.
 # ---------------------
-k=k_read#number of clusters.
 p_init=0        #initial p.
 p_max=0.5       #maximum p.
 p_step=0.01     #p step.
 t_max=500       #maximum number of iterations.
-beta=0.0        #amount of memory for the weight updates.
+#beta=0.0        #amount of memory for the weight updates.
 Restarts=10     #number of MinMax k-means restarts.
 # ---------------------
 
-
-# Cluster the instances using the MinMax k-means procedure.
-# ---------------------------------------------------------
-E_max=np.zeros(shape = (Restarts, 2), dtype = 'float')
-E_sum=np.zeros(shape = (Restarts, 2), dtype = 'float')
-nmi_avg = np.zeros(shape = (Restarts, 2), dtype = 'float')
-
 debug = False
+algorithms = ['MinMax KMeans (β = 0.0)', 'MinMax KMeans (β = 0.1)', 'MinMax KMeans (β = 0.3)', 'KMeans', 'KMeans++']
 
-for repeat in range(0, Restarts):
-    if debug:
-        print('========================================================\n')
-        print(f'MinMax k-means: Restart {repeat+1}\n')
+for dataset in ['pen-based.arff']:#['Coil2.csv', 'iris.arff']:
+    #%%
+    X, Y, N, d, k = utils.read_dataset(dataset)
+
+    #%%
+    # Cluster the instances using the MinMax k-means procedure.
+    # ---------------------------------------------------------
+    E_max=np.zeros(shape = (Restarts, len(algorithms)), dtype = 'float')
+    E_sum=np.zeros(shape = (Restarts, len(algorithms)), dtype = 'float')
+    nmi_avg = np.zeros(shape = (Restarts, len(algorithms)), dtype = 'float')
+    time_avg = np.zeros(shape = (Restarts, len(algorithms)), dtype = 'float')
+
+    for repeat in range(0, Restarts):
+        alg_count = 0
+
+        if debug:
+            print('========================================================\n')
+            print(f'Running all algorithms: Restart {repeat+1}\n')
+        
+        #Randomly initialize the cluster centers.
+        M = utils.get_initial_clusters(X, k, repeat)
     
-    #Randomly initialize the cluster centers.
-    M = utils.get_initial_clusters(X, k, repeat)
-    
-    #Execute MinMax k-means.
-    #Get the cluster assignments, the cluster centers and the cluster variances.
-    Cluster_elem, M, Var_MinMax = mk.MinMax_kmeans(X, M, k, p_init, p_max, p_step, t_max, beta, debug=debug)
-    
-    E_max[repeat, 0]=max(Var_MinMax)
-    E_sum[repeat, 0]=sum(Var_MinMax)
-    nmi_avg[repeat, 0] = normalized_mutual_info_score(Y, Cluster_elem, average_method='arithmetic')
+        for beta in [0.0, 0.1, 0.3]:
+            #Execute MinMax k-means.
+            #Get the cluster assignments, the cluster centers and the cluster variances.
+            start = time.time()
+            Cluster_elem, M, Var_MinMax = mk.MinMax_kmeans(X, M, k, p_init, p_max, p_step, t_max, beta, debug=debug)
+            end = time.time()
 
-    #K-means
-    kmeans = KMeans(n_clusters=k, init=M, max_iter=t_max, n_init=1).fit(X)
-    Dist = dist.cdist(X, kmeans.cluster_centers_, 'sqeuclidean')
-    #Calculate the cluster variances.
-    Var = np.zeros(k)
-    for i in range(0,k):
-        I = np.where(kmeans.labels_ == i)
-        Var[i] = np.sum(Dist[I[0],i])
+            E_max[repeat, alg_count] = max(Var_MinMax)
+            E_sum[repeat, alg_count] = sum(Var_MinMax)
+            nmi_avg[repeat, alg_count] = normalized_mutual_info_score(Y, Cluster_elem, average_method='arithmetic')
+            time_avg[repeat, alg_count] = end - start
 
-    E_max[repeat, 1]=max(Var)
-    E_sum[repeat, 1]=sum(Var)
-    nmi_avg[repeat, 1] = normalized_mutual_info_score(Y, kmeans.labels_, average_method='arithmetic')
+            alg_count += 1
+
+        #K-means
+        start = time.time()
+        kmeans = KMeans(n_clusters=k, init=M, max_iter=t_max, n_init=1).fit(X)
+        end = time.time()
+
+        dist_kmeans = dist.cdist(X, kmeans.cluster_centers_, 'sqeuclidean')
+
+        #Calculate the cluster variances.
+        var_kmeans = np.zeros(k)
+        for i in range(0,k):
+            I = np.where(kmeans.labels_ == i)
+            var_kmeans[i] = np.sum(dist_kmeans[I[0],i])
+
+        E_max[repeat, alg_count]=max(var_kmeans)
+        E_sum[repeat, alg_count]=sum(var_kmeans)
+        nmi_avg[repeat, alg_count] = normalized_mutual_info_score(Y, kmeans.labels_, average_method='arithmetic')
+        time_avg[repeat, alg_count] = end - start
+
+        alg_count += 1
 
 
-    if debug:
-        print(f'End of Restart {repeat+1}\n')
-        print('========================================================\n')
+        #K-means++
+        start = time.time()
+        kmeans_plusplus = KMeans(n_clusters=k, init='k-means++', max_iter=t_max, n_init=1).fit(X)
+        end = time.time()
 
-print(f'Average E_max score over {Restarts+1} restarts: {E_max[:,0].mean()}.')
-print(f'Average E_sum score over {Restarts+1} restarts: {E_sum[:,0].mean()}.')
-print(f'Average nmi score over {Restarts+1} restarts: {nmi_avg[:,0].mean()}.')
+        dist_kmeans_plusplus = dist.cdist(X, kmeans_plusplus.cluster_centers_, 'sqeuclidean')
 
-# ---------------------------------------------------------
+        #Calculate the cluster variances.
+        var_kmeans_plusplus = np.zeros(k)
+        for i in range(0,k):
+            I = np.where(kmeans_plusplus.labels_ == i)
+            var_kmeans_plusplus[i] = np.sum(dist_kmeans_plusplus[I[0],i])
+
+        E_max[repeat, alg_count]=max(var_kmeans_plusplus)
+        E_sum[repeat, alg_count]=sum(var_kmeans_plusplus)
+        nmi_avg[repeat, alg_count] = normalized_mutual_info_score(Y, kmeans_plusplus.labels_, average_method='arithmetic')
+        time_avg[repeat, alg_count] = end - start
+
+        alg_count += 1
+
+        if debug:
+            print(f'Running all algorithms {repeat+1}\n')
+            print('========================================================\n')
+
+    #%%
+    # Summarizing results 
+    summary = pd.DataFrame({'Algorithm': algorithms})
+    summary = summary.assign(e_max = np.mean(E_max, axis=0), e_sum = np.mean(E_sum, axis=0), nmi = np.mean(nmi_avg, axis=0), time = np.mean(time_avg, axis=0))
+
+    print(f'Results for dataset {dataset}')
+    print(tabulate(summary, headers='keys', tablefmt='psql'))
--- UL/ul_project/utils/utils.py
+++ UL/ul_project/utils/utils.py
@@ -104,18 +104,18 @@ def get_initial_clusters(X, k, iter=0):
     M -- Initial cluster [k,:]
     '''
 
-    seed = []
-    seed.append((113, 116, 165))
-    seed.append((26, 195, 165))
-    seed.append((124, 21, 68))
-    seed.append((88, 60, 9))
-    seed.append((108, 186, 74))
-    seed.append((152, 195, 119))
-    seed.append((94, 77, 41))
-    seed.append((13, 71, 105))
-    seed.append((49, 167, 201))
-    seed.append((5, 33, 66))
-
-    #M = X[np.random.choice(X.shape[0], k, replace=False), :]
-    M = X[seed[iter], :]
+    # seed = []
+    # seed.append((113, 116, 165))
+    # seed.append((26, 195, 165))
+    # seed.append((124, 21, 68))
+    # seed.append((88, 60, 9))
+    # seed.append((108, 186, 74))
+    # seed.append((152, 195, 119))
+    # seed.append((94, 77, 41))
+    # seed.append((13, 71, 105))
+    # seed.append((49, 167, 201))
+    # seed.append((5, 33, 66))
+    idxs = np.random.choice(X.shape[0], k, replace=False)
+    M = X[idxs, :]
+    #M = X[seed[iter], :]
     return M
